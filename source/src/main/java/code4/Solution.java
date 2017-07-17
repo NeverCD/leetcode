8a6a9a5da6b108c03b950675f14a52766fa4a3b3
duxf <self.doo@gmail.com>
2017-07-17 23:04:41 +0800

    Remove Duplicates from Sorted Array

---
+++ source/src/main/java/code4/Solution.java
@@ -0,0 +1,11 @@
+package main.java.code4;
+
+/**
+ * Created by duxf on 2017/7/16.
+ */
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double result = 0.0;
+        return result;
+    }
+}
---
+++ source/src/test/java/code26/Test26.java
@@ -0,0 +1,14 @@
+package test.java.code26;
+
+import main.java.code26.Solution;
+
+/**
+ * Created by duxf on 2017/7/17.
+ */
+public class Test26 {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 2, 3, 3, 4};
+        Solution solution = new Solution();
+        solution.removeDuplicates(nums);
+    }
+}
