0d5b5b3c284ab6784309a46fd892ae3723a8d393
duxf <self.doo@gmail.com>
2017-07-19 22:50:23 +0800

    max subarray

---
+++ source/src/main/java/code53/Solution.java
@@ -0,0 +1,15 @@
+package main.java.code53;
+
+/**
+ * Created by duxf on 2017/7/19.
+ */
+public class Solution {
+    public static int maxSubArray(int[] A) {
+        int maxSoFar = A[0], maxEndingHere = A[0];
+        for (int i = 1; i < A.length; ++i) {
+            maxEndingHere = Math.max(maxEndingHere + A[i], A[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
