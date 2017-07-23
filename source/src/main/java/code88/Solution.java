0a9c703bf5b880c573f6d039225d2eb606b48fad
duxf <self.doo@gmail.com>
2017-07-23 22:49:11 +0800

    merge 2 arrays

---
+++ source/src/main/java/code88/Solution.java
@@ -0,0 +1,12 @@
+package main.java.code88;
+
+/**
+ * Created by duxf on 2017/7/23.
+ */
+class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        int i=m-1, j=n-1, k=m+n-1;
+        while (i>-1 && j>-1) A[k--]= (A[i]>B[j]) ? A[i--] : B[j--];
+        while (j>-1)         A[k--]=B[j--];
+    }
+};
