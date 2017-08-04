005bb7c9375acf1ee548b6a4a42177f1ba1a455d
duxf <self.doo@gmail.com>
2017-08-04 22:09:44 +0800

    bit to find missing num; a^b^b = a;

---
+++ source/src/main/java/code268/Solution.java
@@ -0,0 +1,14 @@
+package main.java.code268;
+
+/**
+ * Created by duxf on 2017/8/4.
+ */
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+}
